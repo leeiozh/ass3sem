326c7646c0ccb79b86282fbc1adfd3b8e62131a8
ezhova.ea <ezhova.ea@phystech.edu>
2021-10-21 18:40:19 +0300

    1ex add

---
+++ 1ex.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int tmp = 15;
+int loop = 0;
+
+int main() {
+
+    asm(
+
+    "master:\n"
+    "movl loop, %ebx\n"
+    "addl $1, %ebx\n"
+    "cmpl $33, %ebx\n"
+    "je end\n"
+    "movl %ebx, loop\n"
+
+    "xorl %eax, %eax\n"
+
+    "movl tmp, %r8d\n"
+    "rcll $1, %r8d\n"
+    "movl %r8d, tmp\n"
+
+    "rcll $1, %eax\n"
+    "cmpl $1, %eax\n"
+    "je EQ\n"
+    "jmp NEQ\n"
+
+    "EQ:\n");
+
+    std::cout << 1 << std::endl;
+
+    asm(
+    "jmp master\n"
+    "NEQ:\n");
+    std::cout << 0 << std::endl;
+    asm(
+    "jmp master\n"
+    "end:\n"
+
+    );
+
+    return 0;
+}
+
